417f2248129d0482541928c350ba9af46bdd5a32
lud <ludovic@demblans.com>
2026-03-17 17:34:26 +0100

    chore: Allow Elixir 1.18

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Crown.MixProject do
     [
       app: :crown,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
