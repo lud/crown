1fc366b70a7bb0456b1b83601cf449d359626b44
Ludovic Dem <ludovic@demblans.com>
2026-05-29 19:47:04 +0200

    chore: Fix test helper on fresh booted machine

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,12 @@
-{epmd_state, 0} = System.cmd("epmd", ~w(-names))
-IO.puts(:stderr, epmd_state)
+case System.cmd("epmd", ~w(-names)) do
+  # not running
+  {"", 1} ->
+    :ok
+
+  {epmd_state, 0} ->
+    IO.puts(:stderr, epmd_state)
+end
+
 {_, 0} = System.cmd("epmd", ~w(-daemon))
 :ok = LocalCluster.start()
 
