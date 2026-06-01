678cc2697c5fcdc0e8d57425dbc0474466dc620e
lud <ludovic@demblans.com>
2026-06-01 14:22:50 +0200

    test: Updated Oban to simplify peer mocking

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -4,10 +4,4 @@ Mox.defmock(Crown.OracleMockFull, for: Crown.Oracle)
 # Minimal mock, skips optional callbacks
 Mox.defmock(Crown.OracleMock, for: Crown.Oracle, skip_optional_callbacks: true)
 
-# Behaviour matching the 2-arity convention used by Oban.Peer.safe_call/4 when
-# dispatching to the configured peer module: apply(peer, :leader?, [pid, timeout]).
-defmodule Crown.Test.ObanPeerBehaviour do
-  @callback leader?(GenServer.server(), timeout()) :: boolean()
-end
-
-Mox.defmock(Crown.MockObanPeer, for: [Oban.Peer, Crown.Test.ObanPeerBehaviour])
+Mox.defmock(Crown.MockObanPeer, for: Oban.Peer)
